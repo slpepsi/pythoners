3f15d5e4653a5e2ac42ef1a8ad3c4c9fbb8efc0d
sl <1129052387@qq.com>
2019-08-04 18:39:18 +0800

    add common logging

---
+++ pyutils/commons/logging_settings.py
@@ -0,0 +1,29 @@
+import logging
+import logging.config
+import sys
+from logging.handlers import RotatingFileHandler
+
+
+class MyLogger(object):
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def get_logger(log_name="mylogger.log", log_path="/var/log/"):
+        logger = logging.getLogger(log_name[:-4])
+        formatter = logging.Formatter("%(asctime)s | %(filename)s[line:%(lineno)d] | %(levelname)s | %(message)s")
+
+        # 文件handler
+        file_handler = RotatingFileHandler(log_path + log_name, maxBytes=5 * 1024 * 1024, backupCount=2,
+                                           encoding="utf8")
+        file_handler.setFormatter(formatter)
+        file_handler.setLevel(logging.INFO)
+        logger.addHandler(file_handler)
+
+        console_handler = logging.StreamHandler()
+        console_handler.setFormatter(formatter)
+        console_handler.setLevel(logging.DEBUG)
+        logger.addHandler(console_handler)
+
+        logger.setLevel(logging.DEBUG)
+        return logger
